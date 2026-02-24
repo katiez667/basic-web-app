75e18051735751a22315924c8311cbb54368f8c0
Katie Zhang <zhangmy667@gmail.com>
2026-02-24 15:06:47 +0000

    feat: add number addition and test

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -37,13 +37,11 @@ describe("QueryProcessor", () => {
     const response: string = QueryProcessor(query);
     expect(response).toBe("175");
     });
-    // test('should return andrew id', () => {
-    //     const query = "What is your andrew id?";
-    //     const response: string = QueryProcessor(query);
-    //     expect(response).toBe((
-    //         "mengyua3"
-    //       ));
-    // })
+    test('should return the largest number', () => {
+    const query = "Which of the following numbers is the largest: 28, 62, 84?";
+    const response: string = QueryProcessor(query);
+    expect(response).toBe("84");
+    });
     // test('should return andrew id', () => {
     //     const query = "What is your andrew id?";
     //     const response: string = QueryProcessor(query);
